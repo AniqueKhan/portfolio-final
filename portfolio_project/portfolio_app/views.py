9b1639be1031bf77f956ce724341c5bb6811d1b5
Anique <aniquekhan004@gmail.com>
2023-07-30 14:37:27 +0500

    major changes to use ajax for contact msg

--- portfolio_project/portfolio_app/views.py
+++ portfolio_project/portfolio_app/views.py
@@ -4,40 +4,55 @@ from django.contrib import messages
 from django.core.mail import send_mail
 from .forms import ContactForm
 from django.conf import settings
+from django.http import JsonResponse
 
 
 def index(request):
-    if request.method == "GET":
+    # Home
+    home = Home.objects.latest('updated')
 
-        # Home
-        home = Home.objects.latest('updated')
+    # About
+    about = About.objects.latest('updated')
+    profiles = Profile.objects.filter(about=about)
 
-        # About
-        about = About.objects.latest('updated')
-        profiles = Profile.objects.filter(about=about)
+    # Skills
+    categories = Category.objects.all()
 
-        # Skills
-        categories = Category.objects.all()
+    # Portfolio
+    portfolios = Portfolio.objects.all()
 
-        # Portfolio
-        portfolios = Portfolio.objects.all()
+    # Handle GET request
+    # if request.method == "GET":
+    form = ContactForm()
+    # else:
+    #     form = ContactForm(request.POST)
+    #     if form.is_valid():
+    #         email = form.cleaned_data.get("email")
+    #         name = form.cleaned_data.get("name")
+    #         message = form.cleaned_data.get("message")
 
-        form = ContactForm()
+    #         # You don't need this part here anymore
+    #         # Contact.objects.create(email=email, name=name, message=message)
+    #         # messages.success(request, f"Thank you for reaching out. We will get back to you!")
+    #         # email_subject = "You got a message from your live portfolio website"
+    #         # email_msg = f"Name : {name}\nEmail : {email}\nMessage : {message}"
+    #         # send_mail(subject=email_subject, message=email_msg, from_email=settings.EMAIL_HOST_USER, recipient_list=[settings.EMAIL_HOST_USER])
+    #         # return redirect("index")
 
-        context = {
-            'home': home,
-            'about': about,
-            'profiles': profiles,
-            'categories': categories,
-            'portfolios': portfolios,
-            "form": form
-        }
+    context = {
+        'home': home,
+        'about': about,
+        'profiles': profiles,
+        'categories': categories,
+        'portfolios': portfolios,
+        'form': form
+    }
 
-        return render(request, 'index.html', context)
+    return render(request, 'index.html', context)
+        
 
-    # Contact
-
-    if request.method == "POST":
+def contact_view(request):
+    if request.method == "POST" and request.is_ajax():
         form = ContactForm(request.POST)
         if form.is_valid():
             email = form.cleaned_data.get("email")
@@ -46,12 +61,18 @@ def index(request):
 
             Contact.objects.create(email=email, name=name, message=message)
 
-            messages.success(
-                request, f"Thank you for reaching out. We will get back to you!")
-            
             # Sending email to myself
             email_subject = "You got a message from your live portfolio website"
-            email_msg = f"Name : {name}\nEmail : {email}\nMesage : {message}"
-            send_mail(subject=email_subject,message=email_msg,from_email=settings.EMAIL_HOST_USER,recipient_list=[settings.EMAIL_HOST_USER])
+            email_msg = f"Name : {name}\nEmail : {email}\nMessage : {message}"
+            send_mail(
+                subject=email_subject,
+                message=email_msg,
+                from_email=settings.EMAIL_HOST_USER,
+                recipient_list=[settings.EMAIL_HOST_USER]
+            )
 
-            return redirect("index")
+            return JsonResponse({"success": True})
+        else:
+            return JsonResponse({"errors": form.errors}, status=400)
+    else:
+        return JsonResponse({"error": "Invalid request method"}, status=405)
