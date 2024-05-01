eff4ddb20a99244f1412e77ff82f88ba21e1a968
AniqueKhan <aniquekhan004@gmail.com>
2024-05-01 17:42:51 +0500

    modified media directory

--- portfolio_project/portfolio_project/settings.py
+++ portfolio_project/portfolio_project/settings.py
@@ -125,7 +125,7 @@ STATIC_ROOT = "/home/pynabyte/static_root_directory/static_root_personal_portfol
 
 
 MEDIA_URL = '/media/'
-MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+MEDIA_ROOT ='/home/pynabyte/media_directory/media_directory_personal_portfolio' if ON_VPS else os.path.join(BASE_DIR, 'media')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
