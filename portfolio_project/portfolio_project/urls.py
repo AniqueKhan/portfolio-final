cfe784c9c1a8496db700c2916b4d3e7626895cc8
AniqueKhan <aniquekhan004@gmail.com>
2024-05-01 17:21:16 +0500

    added media urls

--- portfolio_project/portfolio_project/urls.py
+++ portfolio_project/portfolio_project/urls.py
@@ -6,7 +6,5 @@ from django.conf.urls.static import static
 urlpatterns = [
     path('admin/', admin.site.urls),
     path("",include("portfolio_app.urls"))
-]
-if settings.DEBUG:
-    urlpatterns += static(settings.MEDIA_URL,
+] + static(settings.MEDIA_URL,
                           document_root=settings.MEDIA_ROOT)
