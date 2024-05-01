a726886d709d4ca619b9d9b28fb0a507b166f7ac
AniqueKhan <aniquekhan004@gmail.com>
2024-05-01 18:07:01 +0500

    made changes in media root settings

--- portfolio_project/portfolio_project/settings.py
+++ portfolio_project/portfolio_project/settings.py
@@ -124,8 +124,10 @@ STATICFILES_DIRS = [
 STATIC_ROOT = "/home/pynabyte/static_root_directory/static_root_personal_portfolio" if ON_VPS else ''
 
 
-MEDIA_URL = 'http://www.aniqueportfolio.pynabyte.com/media/' if ON_VPS else '/media/'
-MEDIA_ROOT ='/home/pynabyte/media_directory/media_directory_personal_portfolio' if ON_VPS else os.path.join(BASE_DIR, 'media')
+MEDIA_URL ='/media/'
+# MEDIA_URL = 'http://www.aniqueportfolio.pynabyte.com/media/' if ON_VPS else '/media/'
+# MEDIA_ROOT ='/home/pynabyte/media_directory/media_directory_personal_portfolio' if ON_VPS else os.path.join(BASE_DIR, 'media')
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
